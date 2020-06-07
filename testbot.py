9a0776b1a5e3b74aa513a3b1db0fe68dcefd9dd7
asadle <66483775+asadle@users.noreply.github.com>
2020-06-07 21:13:52 +0900

    Update testbot.py

--- testbot.py
+++ testbot.py
@@ -15,17 +15,11 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith("경고누적"):
-    await client.send_message(message.channel, "고유번호를 입력해 주세요")
-
-    if message.content.startswith("76561198969579722"):
-    await client.send_message(message.channel, "샌박기존경고 "3" 회 "누적" "8" "시간벤처리됨")
-    if message.content.startswith("76561199028947184 "):
-    await client.send_message(message.channel, "샌박기존지속적인 룰위반" "8" "벤처리됨")
-    if message.content.startswith("76561199060808072"):
-    await client.send_message(message.channel, "서바이벌 룰위반 경고" "1" "입니다")
-    if message.content.startswith("76561199028524034"):
-    await client.send_message(message.channel, "초식 전투 룰 위반 경고" "1" "회")
+   async def on_message(message):
+    if message.content.startswith("hi"):
+        await client.send_message(message.channel, "HI")
+    if message.content.startswith("아사들"):
+        await client.send_message(message.channel, "왜불러")
     
        
     
